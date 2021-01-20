63865a99b3eec38c53649c096ddd5b21f2976520
emanovic7 <e_owusu@live.com>
2021-01-19 23:33:42 -0500

    cleans up, moves controllers and components into respective files, moves data folder into src folder

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,33 +1,15 @@
 import React, { Component } from 'react';
 import Answer from './Answer';
 
-let data = require('../data/qaData.json');
+//let data = require('../data/qaData.json');
 
 class Question extends Component {
 
     render() {
-        function parseQuestions() {
-            // let hash = {};
-            // data.forEach(data => {
-            //     for(let i=0; i<data.incorrect.length; i++){
-            //         debugger;
-            //         if(hash[data.incorrect[i]] === undefined) {
-            //             hash["incorrect"] = data.incorrect[i];
-            //         }
-            //     }
-            // })
-
-             return data.map(data => {
-               return (<li>{data.question}</li>,
-                <Answer correct={data.correct} incorrect={data.incorrect} />
-               )
-            })
-        }
         
-        console.log("Props in questions", data);
         return(
             <div>
-                <ol>{parseQuestions()}</ol>
+                {/* <ol>{parseQuestions()}</ol> */}
             </div>
         );
     }
