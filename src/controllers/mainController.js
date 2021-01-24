f034d9129a5e8b86aebc49d18a49da83099d0454
emanovic7 <e_owusu@live.com>
2021-01-23 23:00:21 -0500

    adds answerHandlers

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -7,19 +7,43 @@ let data = require('../data/qaData.json');
 
 class MainController extends Component {
 
-    //  parseData(){
-    //     console.log(data[0].question)
-    //     return data.map(question => {
-    //         return <li>{question.question}</li>
-    //     });
-    // }
+    constructor() {
+        super();
+        this.state = {
+            score: 0
+        };
+    }
+
+    handleQuizSubmit = (event) => {
+        event.preventDefault();
+        this.setState({
+            score: 50
+        })
+    };
+
+    handleCorrectAnswer = (value) => {
+        this.setState({
+            score: this.state.score + 10
+        });
+    };
+
+    handleWrongAnswer = (value) => {
+        this.setState({
+            score: this.state.score - 10
+        });
+    };
+
+    handleMultipleChoice = (event) => {
+        let value = event.target.value;
+        
+        value === "correct" ? this.handleCorrectAnswer(value) : this.handleWrongAnswer(value);
+    }
 
     parseSingleQuestion(query) {
         let question = query.question
         let answersHash = {};
         answersHash["correct"] = query.correct
         query.incorrect.map((question, i) => {
-            // debugger;
              answersHash[`incorrect-${i}`] = question
         });
 
@@ -28,51 +52,51 @@ class MainController extends Component {
 
     parseAllQuestions() {
         return data.map(query => {
-            // debugger;
             this.parseSingleQuestion(query)
         })
     };
 
     parseSingleQuestionAndAnswers(question, answersHash) {
-        //debugger;
-        console.log("answers hash", answersHash)
+        console.log(this.state);
         return(
             <li>
                 <div className="question">
                     <p>{question}</p>
                 </div>
 
-                <div> 
-                     <input type="radio" value="correct" id="correct" name="question-answers"/>
-                     <label for="question-answer">
-                         {answersHash.correct}
-                     </label>
-                </div>
-
-                <div> 
-                     <input type="radio" value="incorrect-0" id="incorrect-0" name="question-answers"/>
-                     <label for="question-answer">
-                         {answersHash["incorrect-0"]}
-                     </label>
-                </div>
-
-                <div> 
-                     <input type="radio" value="incorrect-1" id="incorrect-1" name="question-answers"/>
-                     <label for="question-answer">
-                         {answersHash["incorrect-1"]}
-                     </label>
-                </div>
-
-                <div> 
-                     <input type="radio" value="incorrect-2" id="incorrect-2" name="question-answers"/>
+                <div className="multiple-choice" onClick={event => this.handleMultipleChoice(event)}>
+                    <div> 
+                        <input type="radio" value="correct" id="correct" name="question-answers" />
                         <label for="question-answer">
-                            {answersHash["incorrect-2"]}
+                            {answersHash["correct"]}
                         </label>
+                    </div>
+
+                    <div> 
+                        <input type="radio" value="incorrect-0" id="incorrect-0" name="question-answers"/>
+                        <label for="question-answer">
+                            {answersHash["incorrect-0"]}
+                        </label>
+                    </div>
+
+                    <div> 
+                        <input type="radio" value="incorrect-1" id="incorrect-1" name="question-answers"/>
+                        <label for="question-answer">
+                            {answersHash["incorrect-1"]}
+                        </label>
+                    </div>
+
+                    <div> 
+                        <input type="radio" value="incorrect-2" id="incorrect-2" name="question-answers"/>
+                            <label for="question-answer">
+                                {answersHash["incorrect-2"]}
+                            </label>
+                    </div>
                 </div>
             </li> 
         );
         
-    }
+    };
 
     render() {    
 
@@ -82,7 +106,7 @@ class MainController extends Component {
 
         return(
             <div>
-                <form>
+                <form onSubmit={this.handleQuizSubmit}>
                     <ul>{allQuestions}</ul>
                     <input type="submit" value="submit quiz" />
                 </form>    
